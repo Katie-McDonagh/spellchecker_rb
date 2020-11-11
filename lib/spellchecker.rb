99daf624049bd2902d926338f86699ea15a735b4
Tim <timcpb@hotmail.com>
2020-11-11 18:03:56 +0000

    refactor into a class with private methods

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,14 +1,26 @@
-def spellchecker(string) 
-  word_bank = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+class Boiler
+
+  def initialize
+    @word_bank = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+  end
+
+def spellchecker(string)
   #if the string contains strings from the array word bank
   #do nothing but print the original string
   word_array = string.split(" ")
-  checked_word_array = word_array.map { |word| 
-    if word_bank.include?(word) || word_bank.include?(word.downcase)
-      word
-    else 
-      "~#{word}~"
-    end
+  checked_word_array = word_array.map { |word| correct_spelling(word)
   }
   checked_word_array.join(" ")
+end
+
+private
+
+def correct_spelling(word)
+  if @word_bank.include?(word) || @word_bank.include?(word.downcase)
+    word
+  else 
+    "~#{word}~"
+  end
+end
+
 end
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -1,30 +1,34 @@
 require 'spellchecker.rb'
 
 describe "spellchecker" do
+
+  boiler = Boiler.new
+
   it "returns a correctly spelt word" do
-    expect(spellchecker("one")).to eq("one")
+    expect(boiler.spellchecker("one")).to eq("one")
   end
 
   it "returns correctly spelt words" do
-    expect(spellchecker("two")).to eq("two")
+    expect(boiler.spellchecker("two")).to eq("two")
   end
 
   it "returns a highlighted single uncorrectly spelt word" do
-    expect(spellchecker("tww")).to eq("~tww~")
+    expect(boiler.spellchecker("tww")).to eq("~tww~")
   end
 
   it "returns a highlighted single uncorrectly spelt word" do
-    expect(spellchecker("tww")).to eq("~tww~")
+    expect(boiler.spellchecker("tww")).to eq("~tww~")
   end
 
   it "spellchecks a string of muliple words" do
-    expect(spellchecker("one two")).to eq("one two")
-    expect(spellchecker("one tww")).to eq("one ~tww~")
-    expect(spellchecker("one tww three four five Syx Seven eiight nine ten")).to eq("one ~tww~ three four five ~Syx~ Seven ~eiight~ nine ten")
+    expect(boiler.spellchecker("one two")).to eq("one two")
+    expect(boiler.spellchecker("one tww")).to eq("one ~tww~")
+    expect(boiler.spellchecker("one tww three four five Syx Seven eiight nine ten")).to eq("one ~tww~ three four five ~Syx~ Seven ~eiight~ nine ten")
   end
 
   it "spellchecks a capitalized word" do
-    expect(spellchecker("Two")).to eq("Two")
+    expect(boiler.spellchecker("Two")).to eq("Two")
+    expect(boiler.spellchecker("tWo")).to eq("tWo")
   end
 
 end
